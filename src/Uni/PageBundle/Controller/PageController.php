56d9c1552257d785b651fb002347c4f41baeb4cf
gfiguero <gfiguero@gmail.com>
2015-08-17 00:56:13 -0300

    agregada pagina frontal

--- src/Uni/PageBundle/Controller/PageController.php
+++ src/Uni/PageBundle/Controller/PageController.php
@@ -10,4 +10,30 @@ class PageController extends Controller
     {
         return $this->render('UniPageBundle:Page:index.html.twig');
     }
+
+    public function memberAction()
+    {
+        return $this->render('UniPageBundle:Page:member.html.twig');
+    }
+
+    public function historyAction()
+    {
+        return $this->render('UniPageBundle:Page:history.html.twig');
+    }
+
+    public function noticeAction()
+    {
+        return $this->render('UniPageBundle:Page:notice.html.twig');
+    }
+
+    public function reportAction()
+    {
+        return $this->render('UniPageBundle:Page:report.html.twig');
+    }
+
+    public function roleAction()
+    {
+        return $this->render('UniPageBundle:Page:role.html.twig');
+    }
+
 }
