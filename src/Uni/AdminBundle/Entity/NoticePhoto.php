81dd0e98099b81dde5c4586042ec2fe69cb4a8ad
gfiguero <gfiguero@gmail.com>
2015-10-20 19:36:50 -0300

    agregado highlight

--- src/Uni/AdminBundle/Entity/NoticePhoto.php
+++ src/Uni/AdminBundle/Entity/NoticePhoto.php
@@ -111,7 +111,7 @@ class NoticePhoto
     }
     public function upload() { if (null === $this->getFile()) { return; } $generator = new SecureRandom(); $random = $generator->nextBytes(10); $prefix = md5($random); $this->getFile()->move($this->getUploadRootDir(), $prefix.'_'.$this->getFile()->getClientOriginalName());$this->path = $prefix.'_'.$this->getFile()->getClientOriginalName();$this->file = null; }
     public function getAbsolutePath() { return null === $this->path ? null : $this->getUploadRootDir().'/'.$this->path; }
-    public function getWebPath() { return null === $this->path ? 'default' : $this->getUploadDir().'/'.$this->path; }
+    public function getWebPath() { return null === $this->path ? $this->getUploadDir().'/default.png' : $this->getUploadDir().'/'.$this->path; }
     protected function getUploadRootDir() { return __DIR__.'/../../../../web/'.$this->getUploadDir(); }
     protected function getUploadDir() { return '/uploads/notice'; }
 }
