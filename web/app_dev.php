8e8e090c26c70b4cb456a38a3f52930fb3104c3b
gfiguero <gfiguero@gmail.com>
2016-02-03 09:43:27 -0300

    agregadas carpetas paralelas

--- web/app_dev.php
+++ web/app_dev.php
@@ -3,10 +3,10 @@
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Debug\Debug;
 
-$loader = require_once __DIR__.'/../app/bootstrap.php.cache';
+$loader = require_once __DIR__.'/../../unipage/unisign.cl/bootstrap.php.cache';
 Debug::enable();
 
-require_once __DIR__.'/../app/AppKernel.php';
+require_once __DIR__.'/../../unipage/unisign.cl/AppKernel.php';
 
 $kernel = new AppKernel('dev', true);
 $kernel->loadClassCache();
