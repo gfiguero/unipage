f56ef303bc0164fe94db81dba9ebeda86ad4e4de
gfiguero <gfiguero@gmail.com>
2016-04-11 23:55:45 -0300

    reparacion de directorios en los 4 proyectos web

--- web.altoplaceres.cl/app_dev.php
+++ web.altoplaceres.cl/app_dev.php
@@ -3,10 +3,10 @@
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Debug\Debug;
 
-$loader = require_once __DIR__.'/../../unipage/altoplaceres.cl/bootstrap.php.cache';
+$loader = require_once __DIR__.'/../app.altoplaceres.cl/bootstrap.php.cache';
 Debug::enable();
 
-require_once __DIR__.'/../../unipage/altoplaceres.cl/AppKernel.php';
+require_once __DIR__.'/../app.altoplaceres.cl/AppKernel.php';
 
 $kernel = new AppKernel('dev', true);
 $kernel->loadClassCache();
--- web.lago-calafquen.cl/app.php
+++ web.lago-calafquen.cl/app.php
@@ -1,11 +1,9 @@
 <?php
 
-require_once __DIR__.'/../mantencion/index.php'; exit();
-
 use Symfony\Component\ClassLoader\ApcClassLoader;
 use Symfony\Component\HttpFoundation\Request;
 
-$loader = require_once __DIR__.'/../../unipage/lago-calafquen.cl/bootstrap.php.cache';
+$loader = require_once __DIR__.'/../app.lago-calafquen.cl/bootstrap.php.cache';
 
 // Enable APC for autoloading to improve performance.
 // You should change the ApcClassLoader first argument to a unique prefix
@@ -17,7 +15,7 @@ $loader->unregister();
 $apcLoader->register(true);
 */
 
-require_once __DIR__.'/../../unipage/lago-calafquen.cl/AppKernel.php';
+require_once __DIR__.'/../app.lago-calafquen.cl/AppKernel.php';
 //require_once __DIR__.'/../app/AppCache.php';
 
 $kernel = new AppKernel('prod', false);
